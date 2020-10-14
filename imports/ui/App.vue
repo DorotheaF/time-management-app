cda6467b5839c88b6470d64623b72871f99f97e5
LucyB19 <71790667+LucyB19@users.noreply.github.com>
2020-10-14 14:22:04 -0600

    Added Comments

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -45,32 +45,32 @@ export default {
       newTask: "",
       hideCompleted: false
     };
-  },
+  }, //Checkbox for hiding added components starts empty when you first open the website, and the newTask is also set to blank
   methods: {
     handleSubmit(event) {
       Tasks.insert({
         text: this.newTask,
         createdAt: new Date(), // current time
-      });
+      }); 
  
       // Clear form
       this.newTask = "";
-    },
+    }, //This takes a submission to newTask, assigns it to Tasks, and associates the time it was created, and then allows you to put another newTask in
     toggleHideCompleted() {
       this.hideCompleted = !this.hideCompleted;
-    }
+    } // When you change the Completed tasks checkmark, it changes from true to false or from false to true
   },
   meteor: {
     tasks() {
-      let filteredTasks = Tasks.find({}, { sort: { createdAt: -1 } }).fetch();
+      let filteredTasks = Tasks.find({}, { sort: { createdAt: -1 } }).fetch(); //takes tasks, and sorts them in the order that they were created
       if (this.hideCompleted) {
         filteredTasks = filteredTasks.filter(task => !task.checked);
-      }
+      } //If hideCompleted is true, it only filters tasks that are not checked
       return filteredTasks;
     },
     incompleteCount() {
       return Tasks.find({ checked: { $ne: true } }).count();
-    }
+    } //How many tasks are not checked
   }
 };
 </script>
---
+++ imports/ui/MainPage.vue
@@ -0,0 +1,18 @@
+//Main Page
+<template>
+  <div className ="container">
+    <header>
+      <h1> Main Page </h1>
+    </header> 
+      
+  </div>
+</template>
+
+<script>
+  import { Meteor } from "meteor/meteor";
+  import Vue from "vue";
+  import Task from "./Task.vue";
+  import { Tasks } from "../api/tasks.js";
+
+  //export default { }
+</script>
