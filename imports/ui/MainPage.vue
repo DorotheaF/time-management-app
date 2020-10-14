f3c4841bd47e7769c376a41af917dcf34b5a196e
LucyB19 <71790667+LucyB19@users.noreply.github.com>
2020-10-14 16:22:39 -0600

    I'm not even sure

--- imports/ui/MainPage.vue
+++ imports/ui/MainPage.vue
@@ -1,18 +1,16 @@
 //Main Page
 <template>
-  <div className ="container">
-    <header>
+   <div className ="container">
+     <header>
       <h1> Main Page </h1>
     </header> 
-      
-  </div>
+    </div>
 </template>
 
 <script>
   import { Meteor } from "meteor/meteor";
   import Vue from "vue";
-  import Task from "./Task.vue";
-  import { Tasks } from "../api/tasks.js";
+
 
   //export default { }
 </script>
