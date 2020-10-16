7e6fdfb5f3f0e01b1b793e9dc5eebe06dc0853ff
LucyB19 <71790667+LucyB19@users.noreply.github.com>
2020-10-16 13:31:16 -0600

    Deleted MainPage.html

--- client/main.js
+++ client/main.js
@@ -6,10 +6,15 @@ Vue.use(VueMeteorTracker);
  
 import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.vue';
+import MainPage from '. ./imports/ui/MainPage.vue';
  
 Meteor.startup(() => {
   new Vue({
     el: '#app',
     ...App,
   });
+  new Vue({
+    el: '#mainpage',
+    ...MainPage,
+  })
 });
