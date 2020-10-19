da8b1e2d571d5c5187683994351964fd3501992b
gdbouton <71725866+gdbouton@users.noreply.github.com>
2020-10-19 12:49:59 -0600

    new calendar stuff

---
+++ imports/ui/calendarstuff/Calendar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="calendar-month">
+        <div class="calendar-month-header">
+
+            <CalendarDateIndicator 
+            :selected-date="selectedDate"
+            class="calendar-month-header-selected-month"
+            />
+            <CalendarDateSelector
+            :current-date="today"
+            :selected-date="selectedDate"
+            @dateSelected="selectDate"
+            />
+        </div>
+        <CalendarWeekdays />
+
+        <ol class="days-grid">
+            <CalendarMonthDayItem />
+        </ol>
+    </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import CalendarDateIndicator from "./CalendarDateIndicator";
+import CalendarDateSelector from "./CalendarDateSelector";
+import CalendarMonthDayItem from "./CalendarMonthDayItem";
+import CalendarWeekdays from "./CalendarWeekdays";
+
+dayjs.extend(weekday);
+dayjs.extend(weekOfYear);
+export default {
+    name: "Calendar",
+
+    components: {
+        CalendarDateIndicator,
+        CalendarDateSelector,
+        CalendarMonthDayItem,
+        CalendarWeekdays
+    },
+
+    computed: {
+    data() {
+        numberOfDaysInMonth(){
+            return dayjs(this.selectedDate).daysInMonth();
+        }
+    }
+    },
+
+    methods: {
+        selectDate(newSelectedDate) {
+            this.selectedDate=newSelectedDate;
+        }
+    }
+};
+</script>
---
+++ imports/ui/calendarstuff/CalendarDateSelector.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="calendar-date-selector">
+        <span @click="selectPrevious"> Go Back </span>
+        <span @click="selectCurrent">Today</span>
+        <span @click="selectNext">Go Forward</span>
+    </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+export default {
+    name: "CalendarDateSelector",
+
+    props: {
+        currentDate:{
+            type: String,
+            required: true
+        },
+
+        selectedDate: {
+            type: Object,
+            required: true
+        }
+    },
+
+    methods: {
+        selectPrevious() {
+            let newSelectedDate=dayjs(ths.selectedDate).subtract(1,"month");
+            this.$emit("dateSelected", newSelectedDate);
+        },
+        selectCurrent(){
+            let newSelectedDate=dayjs(this.currentDate);
+            this.$emit("dateSelected", newSelectedDate);
+        },
+        selectNext(){
+            let newSelectedDate=dayjs(this.selectedDate).add(1, "month");
+            this.$emit("dateSelected",newSelectedDate);
+        }
+    }
+};
+</script>
