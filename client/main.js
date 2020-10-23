63d9dd4fa8ced6a6907de7be7b8ff509f74d8779
Dorothea French <dorothea.french@gmail.com>
2020-10-23 14:17:02 -0600

    Update main.js

--- client/main.js
+++ client/main.js
@@ -5,26 +5,14 @@ import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.vue';
 import router from './routes'
 
-// Create router instance
 
 Meteor.startup(() => {
+  
   Vue.use(VueMeteorTracker)
+  
   new Vue({
     router,
     render: (h) => h(App),
   }).$mount('#app');
+  
 });
- 
-//Meteor.startup(() => {
-  
-  //new Vue({
-    //route = whatever is in the top bar (or whatever) "www.app.com/xxx"
-    //route =  Calender (or Diagnostic or whatever)
-    //el: '#app',
-    //if route = ""
-    //...MainPage,
-    //if route = calendar 
-    // ...Calendar
-  
-  //});
-//});
