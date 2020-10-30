993248beab2cd45d624f45ef4fa953d8fc837262
Dorothea French <dorothea.french@gmail.com>
2020-10-29 21:12:19 -0600

    Added real current session and changed main page

--- imports/ui/CurrentSession.vue
+++ imports/ui/CurrentSession.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="content">
-    <div class="column">
+    <div class="column-a">
       <div class="priorities">
         <div class="title">
-          Priorities
+          Current Tasks
+        </div>
+        <ul class="task-list" >
+          <Task v-for="(task, index) in prioritiesTaskList" v-bind:key="index" v-bind:task="task" v-bind:index="index+1"/>
+        </ul>
+      </div>
+      <div class="priorities">
+        <div class="title">
+          Finished Tasks
         </div>
         <ul class="task-list" >
           <Task v-for="(task, index) in prioritiesTaskList" v-bind:key="index" v-bind:task="task" v-bind:index="index+1"/>
@@ -18,37 +26,27 @@
         </div>
       </div>
     </div>
-    <div class="column">
-      <div class="mini-calendar-tray">
-        Calendar
-        <div class="mini-calendar">
-          <div class="cal-column">
-            Monday
-            <div class="line" />
-            <div class="cal-item"> Test Task 1 </div> <!-- TODO: Make these responsive-->
-            <div class="cal-item"> Test Task 2 </div>
-          </div>
-          <div class="cal-column">
-            Tuesday
-            <div class="line" />
-          </div>
-          <div class="cal-column">
-            Wednesday
-            <div class="line" />
-            <div class="cal-item"> Test Task 3 </div>
-          </div>
-        </div>
-        
-      </div>
-      <div class="classes-tray">
-        Class List
-        <div class="classes">
-          <div class="class-item"> Subject 1 </div>
-          <div class="class-item"> Subject 2 </div>
-          <div class="class-item"> Subject 3 </div>
-          <div class="class-item"> Subject 4 </div>
-          <div class="class-item"> Subject 5 </div>
-          <div class="class-item"> Subject 6 </div>
+    <div class="column-b">      
+      <div class="break-time-tray">
+        Time until next break <!-- TODO: Finish this!! https://medium.com/js-dojo/how-to-create-an-animated-countdown-timer-with-vue-89738903823f -->
+        <div class="base-timer">
+            <svg
+                class="base-timer__svg"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg">
+            
+                <g class="base-timer__circle">
+                <circle
+                    class="base-timer__path-elapsed"
+                    cx="50"
+                    cy="50"
+                    r="46.5"
+                />
+                </g>
+            </svg>
+            <span class="base-timer__label">
+                {{ formattedTimeLeft }}
+            </span>
         </div>
       </div>
     </div>
@@ -61,30 +59,53 @@ import Task from "./Task.vue"
 import { Meteor } from 'meteor/meteor'
 
 export default {
-  components: {
-    Task
-  },
-  data() {
-    return {
-      proximalTaskList: [],
-      prioritiesTaskList: []
-      /*prioritiesTaskList: [
-        {_id: "ibbaBWC8F7GMvfumM", taskName: "task 3", timeEst: "00:30", dueDate: "10/10/2020", subject: "MCEN 3025"},
-        {_id: "GBdSNWhudZ2m77tvv", taskName: "task 5", timeEst: "00:30", dueDate: "11/1/2020", subject: "GEEN 2400"},
-        {_id: "3KtSh62ParYNKxpgz", taskName: "task 7", timeEst: "00:45", dueDate: "11/05/2020", subject: "MCEN 3025"}
-      ],*/
-    };
-  },
-  created() {
-    Meteor.call('task.returnNextX', (error, result) => { //TODO: add watcher for database, check if component needs to rerender on page reload
-        if (this.prioritiesTaskList!=result){
-          this.prioritiesTaskList = result;
-        }        
-      });
-  },
-  methods: { 
-        
-  }
+    props: {
+        timeLeft: {
+        type: Number,
+        required: true
+        }
+    },
+    components: {
+        Task
+    },
+    data() {
+        return {
+        proximalTaskList: [],
+        prioritiesTaskList: []
+        /*prioritiesTaskList: [
+            {_id: "ibbaBWC8F7GMvfumM", taskName: "task 3", timeEst: "00:30", dueDate: "10/10/2020", subject: "MCEN 3025"},
+            {_id: "GBdSNWhudZ2m77tvv", taskName: "task 5", timeEst: "00:30", dueDate: "11/1/2020", subject: "GEEN 2400"},
+            {_id: "3KtSh62ParYNKxpgz", taskName: "task 7", timeEst: "00:45", dueDate: "11/05/2020", subject: "MCEN 3025"}
+        ],*/
+        };
+    },
+    created() {
+        Meteor.call('task.returnNextX', (error, result) => { //TODO: add watcher for database, check if component needs to rerender on page reload
+            if (this.prioritiesTaskList!=result){
+            this.prioritiesTaskList = result;
+            }        
+        });
+    },
+    computed: {
+        formattedTimeLeft() {
+        const timeLeft = this.timeLeft
+        // The largest round integer less than or equal 
+            //to the result of time divided being by 60.
+        const minutes = Math.floor(timeLeft / 60)
+        // Seconds are the remainder of the time divided
+            //by 60 (modulus operator)
+        let seconds = timeLeft % 60
+        // If the value of seconds is less than 10,
+            //then display seconds with a leading zero
+        if (seconds < 10) {
+            seconds = `0${seconds}`
+        }
+        // The output in MM:SS format
+        return `${minutes}:${seconds}`
+        }
+    },
+    methods: { 
+            
+    }
 }
 </script>
-
--- imports/ui/MainPage.vue
+++ imports/ui/MainPage.vue
@@ -1,87 +1,91 @@
 <template>
-    <div class="mainpage">
-        <div class="content">   
-
-                
-
-            <div class="todo">            
-                <header>
-                <h1>Todo List ({{ incompleteCount }}) </h1>
-                <label class="hide-completed">
-                    <input
-                    type="checkbox"
-                    readOnly
-                    checked="hideCompleted"
-                    v-model="hideCompleted"
-                    @click="toggleHideCompleted"
-                    />
-                    Hide Completed Tasks
-                </label>
-
-                <!-- <blaze-template template="loginButtons" tag="span"></blaze-template> -->
-
-                <form class="new-task" @submit.prevent="handleSubmit">
-                    <input
-                    type="text"
-                    placeholder="Type to add new tasks"
-                    v-model="newTask"
-                    />
-                </form>
-                
-                </header>
-                <ul>
-                <Task v-for="(task, index) in tasks" v-bind:key="task._id" v-bind:task="task" v-bind:yello="index+1"/>
-                </ul>
-
-                <div class="chunkyrow">
-                  
-                  <div class="prioritylist">
-                    <div style ="flex-grow: 1">
-                    <h1> Never gonna give you up, never gonna let you down, never gonna run around and desert you, never gonna make you cry, never gonna say goodbye, never gonna tell a lie and hurt you.</h1>
-                    </div>
-                  </div>
-
-                  <div class="daycalendar">
-                    <div style= "flex-grow: 5">
-                    <h1>Next Three Days 
-                          Monday            Tuesday             Wednesday
-                    </h1>
-                    </div>
-                  </div>
-                  
-              </div>
-              <div class="chunkyrow">
-                <div class=daycalendar>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="">Enter New Task</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="">Enter New Project</a>
-                  </li>
-                  <li class = "nav-item">
-                     <a class="nav-link active" href="">Start New Session</a>
-                  </li>
-                </div>
-
-                <div class=daycalendar>
-                  <h1>All Classes</h1>
-                  <h1>Math</h1>
-                  <h1>English</h1>
-                  <h1>Chemistry. The bane of one's existence</h1>
-                </div>
-              </div>
-                   
-            </div>      
-
-        </div>           
+  <div class="content">
+    <div class="column">
+      <div class="priorities">
+        <div class="title">
+          Priorities
+        </div>
+        <ul class="task-list" >
+          <Task v-for="(task, index) in prioritiesTaskList" v-bind:key="index" v-bind:task="task" v-bind:index="index+1"/>
+        </ul>
+      </div>
+      <div class="new-task">
+        <button class="new-task-button" @click="$router.push('/newTask')">
+          +
+        </button>
+        <div>
+          Add a task
+        </div>
+      </div>
     </div>
-  
+    <div class="column">
+      <div class="mini-calendar-tray">
+        Calendar
+        <div class="mini-calendar">
+          <div class="cal-column">
+            Monday
+            <div class="line" />
+            <div class="cal-item"> Test Task 1 </div> <!-- TODO: Make these responsive-->
+            <div class="cal-item"> Test Task 2 </div>
+          </div>
+          <div class="cal-column">
+            Tuesday
+            <div class="line" />
+          </div>
+          <div class="cal-column">
+            Wednesday
+            <div class="line" />
+            <div class="cal-item"> Test Task 3 </div>
+          </div>
+        </div>
+        
+      </div>
+      <div class="classes-tray">
+        Class List
+        <div class="classes">
+          <div class="class-item"> Subject 1 </div>
+          <div class="class-item"> Subject 2 </div>
+          <div class="class-item"> Subject 3 </div>
+          <div class="class-item"> Subject 4 </div>
+          <div class="class-item"> Subject 5 </div>
+          <div class="class-item"> Subject 6 </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
- 
+
 <script>
+import Task from "./Task.vue"
+import { Meteor } from 'meteor/meteor'
+
 export default {
+  components: {
+    Task
+  },
+  data() {
+    return {
+      proximalTaskList: [],
+      prioritiesTaskList: []
+      /*prioritiesTaskList: [
+        {_id: "ibbaBWC8F7GMvfumM", taskName: "task 3", timeEst: "00:30", dueDate: "10/10/2020", subject: "MCEN 3025"},
+        {_id: "GBdSNWhudZ2m77tvv", taskName: "task 5", timeEst: "00:30", dueDate: "11/1/2020", subject: "GEEN 2400"},
+        {_id: "3KtSh62ParYNKxpgz", taskName: "task 7", timeEst: "00:45", dueDate: "11/05/2020", subject: "MCEN 3025"}
+      ],*/
+    };
+  },
+  created() {
+    Meteor.call('task.returnNextX', (error, result) => { //TODO: add watcher for database, check if component needs to rerender on page reload
+        if (this.prioritiesTaskList!=result){
+          this.prioritiesTaskList = result;
+        }        
+      });
+  },
+  methods: { 
+        
+  }
 }
 </script>
 
-<style>
-</style>
+
