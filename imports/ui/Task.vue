3ef98cdcd104bdf8abce9aac6dffd165ca101b7b
Dorothea French <dorothea.french@gmail.com>
2020-11-09 13:02:32 -0700

    Update Task.vue

--- imports/ui/Task.vue
+++ imports/ui/Task.vue
@@ -8,6 +8,12 @@
       <div class="title">
         {{ this.task.taskName }}
       </div>
+      <div class="due-label-late" v-if="calcDate() <=0"> <!-- TODO: Style these -->
+        {{ " " }} Days until due: {{ calcDate() }}
+      </div>
+      <div class="due-label" v-else>
+        {{ " :) " }} Days until due: {{ calcDate() }}
+      </div>
     </div>   
     <div class="time-est">
       {{ this.task.timeEst }}
@@ -26,7 +32,13 @@ export default {
     return {};
   },
   methods: {
-   
+    calcDate() {
+      this.task.dueDate 
+      var today = new Date();
+      var due = new Date(this.task.dueDate);
+      var time_diff = Math.round((due.getTime() - today.getTime())/(1000*3600*24))
+      return time_diff;
+    }
   }
 };
 </script>
