9d77e9164c6d6dbec423036d2b18d3742a944c59
Dorothea French <dorothea.french@gmail.com>
2020-11-11 14:30:34 -0700

    Create CalTask2.vue

---
+++ imports/ui/CalTask2.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="text" >      
+        {{ show() }}  
+      <div class="title" v-if="render==true">
+        {{ this.task.taskName }}      
+        </div>  
+    </div>  
+
+  
+</template>
+ 
+<script>
+import { Tasks } from "../api/tasks.js";
+ 
+export default {
+  props: ["task", "day"],
+  data() { 
+    return {
+        render: false,
+        day2: ""
+    };
+  },
+  methods: {
+    show() {      
+      var due = this.task.dueDate;
+      var day = new Date(this.day.date);
+      if (due.getTime() == day.getTime()){
+          this.render = true;
+      }
+    }
+  }
+};
+</script>
